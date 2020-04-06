b3e0f800daafbcc7e70948395b120d687444b5fc
zsxoff <zsxoff@gmail.com>
2020-04-06 04:28:46 +0400

    cleanup code

--- laboratory-1/src/matrix_lib.cpp
+++ laboratory-1/src/matrix_lib.cpp
@@ -10,18 +10,9 @@ const std::vector<std::vector<float>> matmul(
     const std::vector<std::vector<float>>& A,
     const std::vector<std::vector<float>>& B) {
     // Get matrix sizes.
-    const unsigned int A_ROWS = A.size();
-    const unsigned int A_COLS = A[0].size();
-
-    const unsigned int B_ROWS = B.size();
-    const unsigned int B_COLS = B[0].size();
-
-    // TODO(zsxoff): Possibly check sizes.
-
-    // Synonims to sizes.
-    const size_t L = A_ROWS;
-    const size_t M = A_COLS;
-    const size_t N = B_COLS;
+    const size_t L = A.size();     // A Rows
+    const size_t M = A[0].size();  // A Cols
+    const size_t N = B[0].size();  // B Cols
 
     // Init new vector with sizes M rows, N cols.
     std::vector<std::vector<float>> C;
