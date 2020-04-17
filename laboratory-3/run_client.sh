a981552a0d8be4502df5c1a010a0739feaa83079
zsxoff <zsxoff@gmail.com>
2020-04-17 05:42:56 +0400

    remove unused script

--- laboratory-3/run_client.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-IP=0.0.0.0
-PORT=7777
-
-./laboratory-3 -mode=client -port=${PORT} -ip=${IP}
--- laboratory-3/run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-IP=0.0.0.0
-PORT=7777
-
-./laboratory-3 -mode=server -port=${PORT} -ip=${IP}
