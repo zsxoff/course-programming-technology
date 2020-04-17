978c2530fc94b5af122f7621b3783d7257fe6d92
zsxoff <zsxoff@gmail.com>
2020-04-17 04:03:53 +0400

    add run scripts

---
+++ laboratory-3/run_client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+IP=0.0.0.0
+PORT=7777
+
+./laboratory-3 -mode=client -port=${PORT} -ip=${IP}
---
+++ laboratory-3/run_server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+IP=0.0.0.0
+PORT=7777
+
+./laboratory-3 -mode=server -port=${PORT} -ip=${IP}
